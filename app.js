517a364b403a490d81a4bd69b7ade0f286b8fd76
shouvik <shouvikkhanra1@gmail.com>
2023-03-11 00:42:04 +0530

    add item

--- app.js
+++ app.js
@@ -4,35 +4,28 @@ const app = express()
 const port = 3000
 
 app.set("view engine","ejs")
-
+app.use(bodyParser.urlencoded({extended: true}))
+var items = []
 app.get('/', (req, res) => {
   var today = new Date();
-  var day = ""
-  if(today.getDay()==6){
-    day = "saturday"
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
   }
-  else if(today.getDay()==0){
-    day = "sunday"
-  }
-  else if(today.getDay()==1){
-    day = "monday"
-  }
-  else if(today.getDay()==2){
-    day = "tuesday"
-  }
-  else if(today.getDay()==3){
-    day = "wednesday"
-  }
-  else if(today.getDay()==4){
-    day = "thursday"
-    
-  }
-  else if(today.getDay()==5){
-    day = "friday"
-  }
-  res.render('list',{kindOfDay: day})
+
+  day = today.toLocaleDateString("en-US",options)
 
   
+  res.render('list',{kindOfDay: day,nameList: items})
+
+  
+})
+
+app.post("/",(req,res) => {
+  var box = req.body.newItem
+  items.push(box)
+  res.redirect("/")
 })
 
 
