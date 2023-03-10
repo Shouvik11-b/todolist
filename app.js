d32a969d2df163e98d851ee5f2139d7f2c09fc13
shouvik <shouvikkhanra1@gmail.com>
2023-03-10 20:02:06 +0530

    find day using if else

--- app.js
+++ app.js
@@ -1,16 +1,41 @@
-const express = require('express')
+const express = require('express');
+const bodyParser = require('body-parser');
 const app = express()
 const port = 3000
 
+app.set("view engine","ejs")
+
 app.get('/', (req, res) => {
   var today = new Date();
-  if(today.getDate==6 || today.getDate==0){
-    res.send("weekend");
+  var day = ""
+  if(today.getDay()==6){
+    day = "saturday"
   }
-  else{
-    res.send("not weekend");
+  else if(today.getDay()==0){
+    day = "sunday"
   }
+  else if(today.getDay()==1){
+    day = "monday"
+  }
+  else if(today.getDay()==2){
+    day = "tuesday"
+  }
+  else if(today.getDay()==3){
+    day = "wednesday"
+  }
+  else if(today.getDay()==4){
+    day = "thursday"
+    
+  }
+  else if(today.getDay()==5){
+    day = "friday"
+  }
+  res.render('list',{kindOfDay: day})
+
+  
 })
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
